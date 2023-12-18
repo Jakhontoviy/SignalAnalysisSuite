90999247dc3fbad8257df37e6abca1e4646d805f
Roman Purtov <rytrycon@gmail.com>
2023-12-19 01:03:37 +0700

    Update wavelet_analyzer.py

--- wavelet_analyzer.py
+++ wavelet_analyzer.py
@@ -4,68 +4,87 @@ import matplotlib.pyplot as plt
 import pywt
 import argparse
 
-# Константы для настройки алгоритма
-WAVELET_TYPE = 'db4' # Добеши 4 - тип вейвлета для преобразования
+# Установка типа вейвлета для преобразования
+WAVELET_TYPE = 'db4' # Добеши 4 - выбран из-за его свойств для анализа сигналов
 
-# Настройка аргументов командной строки
+# Настройка аргументов командной строки для ввода пути к файлу с данными
 parser = argparse.ArgumentParser(description='Скрипт обработки сигналов с использованием вейвлет-преобразования')
 parser.add_argument('file_path', help='Путь к файлу Excel, содержащему данные сигнала')
 args = parser.parse_args()
 
-# Функция для загрузки данных из Excel
+# Функция для загрузки данных из файла Excel
 def load_signal_data(file_path):
+    # Попытка загрузить данные из файла Excel
     try:
         data = pd.read_excel(file_path)
+        # Преобразование строковых значений в числовые, замена запятых на точки
         md = np.array(data['Ref, m'].apply(lambda x: float(str(x).replace(',', '.'))))
         gr_val = np.array(data['Signal'].apply(lambda x: float(str(x).replace(',', '.'))))
-    except ValueError:
+    except ValueError:  # Если столбцы не имеют заголовков
         data = pd.read_excel(file_path, header=None)
         md = np.array(data.iloc[:, 0].apply(lambda x: float(str(x).replace(',', '.'))))
         gr_val = np.array(data.iloc[:, 1].apply(lambda x: float(str(x).replace(',', '.'))))
     return md, gr_val
 
-# Функция для вейвлет-преобразования и восстановления сигнала
-def wavelet_transform_and_reconstruct(md, gr_val):
-    # Вейвлет-преобразование
+# Функция для выполнения вейвлет-преобразования и определения интервалов
+def wavelet_transform_and_define_intervals(md, gr_val):
+    # Выполнение вейвлет-преобразования сигнала
     coeffs = pywt.wavedec(gr_val, WAVELET_TYPE)
-    
-    # Обнуление малозначимых коэффициентов
+    # Определение порогового значения для фильтрации коэффициентов
     threshold = np.sqrt(2 * np.log(len(gr_val))) * np.median(np.abs(coeffs[-1])) / 0.6745
+    # Применение порога к коэффициентам
     new_coeffs = list(map(lambda x: pywt.threshold(x, threshold, mode='soft'), coeffs))
-    
-    # Обратное вейвлет-преобразование
-    reconstructed_signal = pywt.waverec(new_coeffs, WAVELET_TYPE)
 
-    return reconstructed_signal[:len(md)]
+    # Определение точек изменения на основе детализированных коэффициентов
+    detail_coeffs = new_coeffs[1:]  # Исключение аппроксимационных коэффициентов
+    change_points = set()
+    max_index = len(md) - 1  # Определение максимально возможного индекса
 
-# Визуализация исходного и восстановленного сигнала
-def plot_and_save(md, original_signal, reconstructed_signal):
+    for i, level_coeffs in enumerate(detail_coeffs):
+        scale_factor = 2 ** i
+        significant_points = np.where(np.abs(level_coeffs) > threshold)[0]
+        for point in significant_points:
+            # Преобразование индекса в реальное положение в исходном сигнале
+            actual_point = min(point * scale_factor, max_index)
+            change_points.add(actual_point)
+
+    # Сортировка точек изменения для создания интервалов
+    intervals = sorted(list(change_points))
+    return new_coeffs, intervals
+
+# Функция для восстановления сигнала и его визуализации
+def reconstruct_and_visualize(md, gr_val, new_coeffs, intervals):
+    # Обратное вейвлет-преобразование для восстановления сигнала
+    reconstructed_signal = pywt.waverec(new_coeffs, WAVELET_TYPE)[:len(md)]
+
+    # Построение графиков исходного и восстановленного сигналов
     plt.figure(figsize=(12, 6))
-    plt.plot(md, original_signal, label='Исходный сигнал', color='blue')
+    plt.plot(md, gr_val, label='Исходный сигнал', color='blue')
     plt.plot(md, reconstructed_signal, label='Восстановленный сигнал', linestyle='--', color='red')
+
+    # Отображение интервалов на графике
+    for interval in intervals:
+        plt.axvline(x=md[interval], color='lightgray', linestyle='dotted', linewidth=0.5)
+
     plt.title('Сравнение исходного и восстановленного сигналов через вейвлет-преобразование')
     plt.xlabel('Глубина, м')
     plt.ylabel('Значение сигнала')
     plt.legend()
     plt.grid(True)
-    plt.savefig('wavelet_signal_comparison.png')
     plt.show()
 
-# Функция для сохранения дополнительной информации
-def save_wavelet_analysis_info(file_path, original_signal, reconstructed_signal):
+# Функция для сохранения интервалов в текстовый файл
+def save_intervals_to_txt(intervals, md, file_path):
+    # Преобразование индексов интервалов в соответствующие глубины
+    interval_values = [md[i] for i in intervals]
+    
+    # Запись значений интервалов в файл
     with open(file_path, 'w', encoding='utf-8') as file:
-        file.write("Разница между исходным и восстановленным сигналом (массив):\n")
-        difference_array = original_signal - reconstructed_signal
-        file.write(str(difference_array) + "\n\n")
-
-        file.write("Средняя разница между исходным и восстановленным сигналом:\n")
-        mean_difference = np.mean(np.abs(difference_array))
-        file.write(str(mean_difference) + "\n")
+        for value in interval_values:
+            file.write(f"{value}\n")
 
 # Основной блок скрипта
-md, gr_val = load_signal_data(args.file_path)
-reconstructed_signal = wavelet_transform_and_reconstruct(md, gr_val)
-plot_and_save(md, gr_val, reconstructed_signal)
-
-# Сохранение дополнительной информации
-save_wavelet_analysis_info("wavelet_analysis_info.txt", gr_val, reconstructed_signal)
+md, gr_val = load_signal_data(args.file_path)  # Загрузка данных
+new_coeffs, intervals = wavelet_transform_and_define_intervals(md, gr_val)  # Выполнение вейвлет-преобразования и определение интервалов
+reconstruct_and_visualize(md, gr_val, new_coeffs, intervals)  # Визуализация результатов
+save_intervals_to_txt(intervals, md, 'intervals.txt')  # Сохранение интервалов в файл
